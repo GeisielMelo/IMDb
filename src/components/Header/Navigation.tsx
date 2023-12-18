07cb581aec826ff34289eb01174d65823682e227
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-18 17:45:52 -0300

    fix: fixed z-index

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -9,7 +9,7 @@ const Navigation: React.FC = () => {
     <nav className='flex justify-between items-center px-6 py-2'>
       <img className='h-8' src={logo} alt='Logo' />
 
-      <div className={`absolute md:static bg-white left-0 top-12 md:w-auto w-full flex items-center px-6 ${!isOpen && 'hidden'} md:block`}>
+      <div className={`absolute md:static bg-white left-0 top-12 md:w-auto w-full flex items-center px-6 ${!isOpen && 'hidden'} md:block z-20`}>
         <ul className='flex md:flex-row flex-col md:items-center md:gap-[4vw] py-2 gap-8'>
           <li className='whitespace-nowrap'>Home</li>
           <li className='whitespace-nowrap '>TV Show</li>
