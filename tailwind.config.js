d8ae772ab8dc7b68867848f596fad164552fe4f5
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-17 23:58:24 -0300

    raw: created custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss'
+
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      ...defaultTheme.screens,
+    },
+    plugins: [],
   },
-  plugins: [],
 }
