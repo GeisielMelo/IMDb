bc54d2841c3737a6dc0f774181fcf836571fb532
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-19 18:30:30 -0300

    feat: Made the preview responsive.

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -24,29 +24,38 @@ const Preview: React.FC<Data> = ({ setPreviewData, elementData }) => {
   }
 
   return (
-    <div className='fixed top-0 flex justify-center items-center h-screen w-screen z-30 backdrop-blur-md px-4'>
-      <div className='relative max-w-[700px] flex p-8 rounded-lg shadow bg-white'>
+    <div className='fixed top-0 flex justify-center items-center h-[100dvh] w-screen z-30 backdrop-blur-md px-4'>
+      <div className='relative max-w-[560px] md:max-w-[700px] md:flex p-8 rounded-lg shadow bg-white'>
         <img
-          className='max-h-80 aspect-[9/16] object-cover'
+          loading='lazy'
+          className='hidden md:block	md:max-h-80 md:aspect-[9/16] md:object-cover'
           src={handlePosterPath(elementData.poster_path || elementData.backdrop_path)}
           alt={elementData.name || elementData.title || elementData.original_title || elementData.original_name}
         />
-        <div className='flex flex-col ml-4'>
+        {elementData.backdrop_path && (
+          <img
+            loading='lazy'
+            className='md:hidden'
+            src={handlePosterPath(elementData.backdrop_path)}
+            alt={elementData.name || elementData.title || elementData.original_title || elementData.original_name}
+          />
+        )}
+        <div className='flex flex-col mt-4 md:ml-4 md:mt-0'>
           <h1 className='font-sans font-medium'>
             {elementData.name || elementData.title || elementData.original_title || elementData.original_name}
             {' - '}
             {handleFormatToYear(elementData.release_date ?? elementData.first_air_date ?? '')}
           </h1>
 
-          <p className='py-4'>{elementData.overview}</p>
+          <p className='py-4 mb-8'>{elementData.overview}</p>
 
           <>
             <div className='absolute bottom-8 flex gap-2'>
-              <img className='w-12' src={IMDB} alt='IMDB Logo' />
+              <img loading='lazy' className='w-12' src={IMDB} alt='IMDB Logo' />
               <span>{handleFormatVoteAverage(elementData.vote_average)}</span>
             </div>
             <div className='absolute bottom-8 right-8 flex gap-2'>
-              <button>
+              <button className='bg-transparent'>
                 <Eye />
               </button>
               <button>
