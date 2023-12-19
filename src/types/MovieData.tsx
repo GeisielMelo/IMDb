938b28b6a9829e9055f88ff158f751a804bce6c2
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-19 17:36:11 -0300

    feat: Created a types file with some types.

---
+++ src/types/MovieData.tsx
@@ -0,0 +1,18 @@
+export type MovieData = {
+  backdrop_path: string | null
+  poster_path: string | null
+  original_language: string
+  original_title?: string
+  original_name?: string
+  overview: string
+  release_date?: string
+  first_air_date?: string
+  title?: string
+  name?: string
+  video?: boolean
+  adult: boolean
+  genre_ids: number[]
+  vote_average: number
+  vote_count: number
+  popularity: number
+}
