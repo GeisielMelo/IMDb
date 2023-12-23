da354dee50de69026619728daa73ea3f8b7ba0d2
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-23 00:14:16 -0300

    feat: Created CRUD operations for Firebase.

--- src/services/Firebase.tsx
+++ src/services/Firebase.tsx
@@ -1,19 +1,51 @@
-import { doc, setDoc, collection, serverTimestamp } from 'firebase/firestore'
+import {
+  doc,
+  setDoc,
+  collection,
+  serverTimestamp,
+  getDoc,
+  updateDoc,
+} from 'firebase/firestore'
 import { database } from '../config/firebase'
 
 const collectionRef = collection(database, 'repos')
 
-const create = async (uid: string) => {
+export const show = async (uid: string) => {
+  try {
+    const docRef = doc(collectionRef, uid)
+    const docSnap = await getDoc(docRef)
+    return docSnap.data()
+  } catch (error) {
+    console.error('Database Error: Failed to retrieve record.')
+  }
+}
+
+export const create = async (uid: string) => {
   const repository = {
     uid,
+    movies: [],
     createdAt: serverTimestamp(),
     lastUpdate: serverTimestamp(),
   }
 
   try {
-    const repositoryRef = doc(collectionRef, repository.uid)
+    const repositoryRef = doc(collectionRef, uid)
     await setDoc(repositoryRef, repository)
   } catch (error) {
-    console.error(error)
+    console.error('Database Error: Failed to create new database.')
+  }
+}
+
+export const update = async (data: object[], uid: string) => {
+  const updatedRef = {
+    movies: data,
+    lastUpdate: serverTimestamp(),
+  }
+
+  try {
+    const repositoryRef = doc(collectionRef, uid)
+    await updateDoc(repositoryRef, updatedRef)
+  } catch (error) {
+    console.error('Database Error: Failed to update record.')
   }
 }
