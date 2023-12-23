73e7ed672670afbc8c8e3f1aafedd602df431172
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-23 00:28:09 -0300

    feat: Updated MovieContext to handle new Firebase CRUD operations.

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -1,4 +1,12 @@
-import { ReactNode, createContext, useContext, useState } from 'react'
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from 'react'
+import { show, create, update } from '../services/Firebase'
+import { useAuth } from './AuthContext'
 import { MovieData } from '../types/MovieData'
 
 type MovieProviderProps = {
@@ -23,14 +31,47 @@ export const useMovies = () => {
 }
 
 export const MovieProvider: React.FC<MovieProviderProps> = ({ children }) => {
+  const { authenticated, user } = useAuth()
   const [movies, setMovies] = useState<MovieData[] | []>([])
 
+  useEffect(() => {
+    const fetchHistory = async () => {
+      if (user) {
+        try {
+          const data = await show(user.uid)
+          if (data) {
+            if (data.movies) setMovies(data.movies)
+          } else {
+            await create(user.uid)
+          }
+        } catch (error) {
+          console.error('An error occurred while fetching the data.')
+        }
+      }
+    }
+    fetchHistory()
+  }, [user])
+
+  const syncData = async () => {
+    try {
+      if (user) await update(movies, user.uid)
+    } catch (error) {
+      console.error('An error occurred while synchronizing the data.')
+    }
+  }
+
   const addMovie = (data: MovieData) => {
+    if (!authenticated)
+      throw Error('You must be connected to perform this action.')
     setMovies((prev) => [...prev, data])
+    syncData()
   }
 
   const removeMovie = (data: MovieData) => {
+    if (!authenticated)
+      throw Error('You must be connected to perform this action.')
     setMovies((prev) => prev.filter((movie: MovieData) => movie.id !== data.id))
+    syncData()
   }
 
   const value: MoviesContextProps = { movies, addMovie, removeMovie }
