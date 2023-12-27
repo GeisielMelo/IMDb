2dc99429770cab81c28c034cc0a8ce6cc45a18eb
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-27 00:12:30 -0300

    feat: added footer

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,20 +3,24 @@ import Navigation from '../components/Header/Navigation'
 import Slider from '../components/Carousel/Slider'
 import ListSlider from '../components/Carousel/ListSlider'
 import { useMovies } from '../context/MovieContext'
+import Footer from '../components/Footer/Footer'
 
 const Index: React.FC = () => {
-  const { movies } = useMovies()
+  const { movies, loading } = useMovies()
 
   return (
     <>
       <Navigation />
       <section>
-        {!!movies.length && <ListSlider category='My List' data={movies} />}
+        {!!movies.length && (
+          <ListSlider category='My List' data={movies} loading={loading} />
+        )}
         <Slider category={'Trending'} url={url.trending} options={options} />
         <Slider category={'Upcoming'} url={url.upcoming} options={options} />
         <Slider category={'Movies'} url={url.movies} options={options} />
         <Slider category={'Tv Show'} url={url.tvShow} options={options} />
       </section>
+      <Footer/>
     </>
   )
 }
