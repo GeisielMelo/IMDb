06b841925f6961e66712bdd77e8a33dcb9df5c89
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-27 00:12:47 -0300

    style: lint fix

--- src/components/Carousel/Image.tsx
+++ src/components/Carousel/Image.tsx
@@ -14,16 +14,9 @@ export const Image: React.FC<TImage> = ({ onClick, src, alt }) => {
   }
 
   return (
-    <div
-      className='relative mx-[0.25rem] aspect-[9/13] hover:cursor-pointer'
-      onClick={onClick}
-    >
-      <img src={src} alt={alt} onLoad={handleImageLoad} className='aspect-[9/13]'/>
-      <div
-        className={`absolute top-0 left-0 w-full h-full bg-slate-400 ${
-          isImageLoaded && 'hidden'
-        }`}
-      >
+    <div className='relative mx-[0.25rem] aspect-[9/13] hover:cursor-pointer' onClick={onClick}>
+      <img src={src} alt={alt} onLoad={handleImageLoad} className='aspect-[9/13]' />
+      <div className={`absolute top-0 left-0 w-full h-full bg-slate-400 ${isImageLoaded && 'hidden'}`}>
         <div className='animate-pulse w-full h-full bg-slate-500' />
       </div>
     </div>
