32945157a7eb9aef024892212c682dd29d9cc96c
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-28 00:18:52 -0300

    feat: Removed list slider from the main page.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,20 +1,14 @@
 import { options, url } from '../config/themoviedb'
 import Navigation from '../components/Header/Navigation'
 import Slider from '../components/Carousel/Slider'
-import ListSlider from '../components/Carousel/ListSlider'
-import { useMovies } from '../context/MovieContext'
 import Footer from '../components/Footer/Footer'
 
 const Index: React.FC = () => {
-  const { movies, loading } = useMovies()
 
   return (
     <>
       <Navigation />
       <section>
-        {!!movies.length && (
-          <ListSlider category='My List' data={movies} loading={loading} />
-        )}
         <Slider category={'Trending'} url={url.trending} options={options} />
         <Slider category={'Upcoming'} url={url.upcoming} options={options} />
         <Slider category={'Movies'} url={url.movies} options={options} />
