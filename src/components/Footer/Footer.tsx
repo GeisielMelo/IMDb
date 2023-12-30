2f46d2755c138b9c9d8aafb243676ee5db4c0586
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-30 16:38:33 -0300

    refactor: Refactored the footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Footer: React.FC = () => {
+type IFooter = {
+  fixed?: boolean
+}
+
+const Footer: React.FC<IFooter> = ({fixed = false}) => {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className='flex items-center justify-center mt-4'>
-      <p className='p-6'>
+    <footer className={`${fixed && 'fixed bottom-0 w-full'} flex items-center justify-center mt-4`}>
+      <p className='p-6 text-center'>
         &copy; {currentYear}{' '}
         <a href='https://github.com/GeisielMelo' target='_blank'>
           GeisielMelo
