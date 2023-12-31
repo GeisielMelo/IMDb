fa1d4939dae375fbfdc2b72ddaf654bfe7fe676c
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-12-30 23:51:22 -0300

    raw: updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
-import defaultTheme from 'tailwindcss'
-
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    screens: {
-      xs: '475px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      ...defaultTheme.screens,
-    },
-    extend: {
-      colors: {
-        'transparent-silver': 'rgba(200,200,200,0.5)',
-      },
-      boxShadow: {
-        'custom': '0 1px 7px 2px rgba(0, 0, 0, 0.3)',
-      },
-    },
-    plugins: [],
+    extend: {},
   },
+  plugins: [],
 }
