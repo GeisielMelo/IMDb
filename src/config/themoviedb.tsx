9378f80195d4a28624c11692ac342e40abcc0cd9
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-01 17:08:44 -0300

    refactor: Moved URLs to the Home.tsx component.

--- src/config/themoviedb.tsx
+++
@@ -1,8 +0,0 @@
-type TUrl = { [key: string]: string }
-
-export const url: TUrl = {
-  trending: 'https://api.themoviedb.org/3/trending/all/day?language=en-US',
-  movies: 'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
-  tvShow: 'https://api.themoviedb.org/3/trending/tv/day?language=en-US',
-  upcoming: 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1',
-}
--- src/pages/Main/Home.tsx
+++ src/pages/Main/Home.tsx
@@ -1,17 +1,23 @@
-import { options, url } from '../../config/themoviedb'
 import Navigation from '../../components/Header/Navigation'
 import Slider from '../../components/Carousel/Slider'
 import Footer from '../../components/Footer/Footer'
 
 const Home: React.FC = () => {
+  const locale = navigator.language
+  const url = 'https://api.themoviedb.org/3/'
+  const trending = `${url}trending/all/day?language=${locale}`
+  const movies = `${url}trending/movie/day?language=${locale}`
+  const tvShow = `${url}trending/tv/day?language=${locale}`
+  const upcoming = `${url}movie/upcoming?language=${locale}&page=1`
+
   return (
     <>
       <Navigation />
       <section className='py-10'>
-        <Slider category={'Trending'} url={url.trending} options={options} />
-        <Slider category={'Upcoming'} url={url.upcoming} options={options} />
-        <Slider category={'Movies'} url={url.movies} options={options} />
-        <Slider category={'Tv Show'} url={url.tvShow} options={options} />
+        <Slider category={'Trending'} url={trending} />
+        <Slider category={'Upcoming'} url={upcoming} />
+        <Slider category={'Movies'} url={movies} />
+        <Slider category={'Tv Show'} url={tvShow} />
       </section>
       <Footer />
     </>
