88b376e5b9a889e05095c210dcd8d9ba8a57559b
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-02 17:53:29 -0300

    feat: Implemented a method to fetch all data and render a page based on it.

--- src/pages/Main/Movies.tsx
+++ src/pages/Main/Movies.tsx
@@ -1,6 +1,10 @@
-import { useParams } from 'react-router-dom'
 import Navigation from '../../components/Header/Navigation'
-import Movie from '../../components/Movie/Movie'
+import Title from '../../components/Title/Title'
+import Footer from '../../components/Footer/Footer'
+import { useParams } from 'react-router-dom'
+import { useFetchTMDB } from '../../hooks/useFetchTMDB'
+import { ReleaseData } from '../../components/Title/types/ReleaseData'
+import { TitleData } from '../../components/Title/types/TitleData'
 
 type IParams = {
   id?: string
@@ -8,13 +12,42 @@ type IParams = {
 
 const Movies: React.FC = () => {
   const params = useParams<IParams>()
+  const locale: string = navigator.language
+  const id = params.id
+
+  const urls = {
+    data: `https://api.themoviedb.org/3/movie/${id}?language=${locale}`,
+    release: `https://api.themoviedb.org/3/movie/${id}/release_dates`,
+    credits: `https://api.themoviedb.org/3/movie/${id}/credits?language=${locale}`,
+    reviews: `https://api.themoviedb.org/3/movie/${id}/reviews?language=${locale}&page=1`,
+    similar: `https://api.themoviedb.org/3/movie/${id}/similar?language=${locale}&page=1`,
+    videos: `https://api.themoviedb.org/3/movie/${id}/videos?language=${locale}`,
+  }
+
+  const { data: title, loading: titleLoading, error: titleError } = useFetchTMDB<TitleData>(urls.data)
+  const { data: release, loading: releaseLoading, error: releaseError } = useFetchTMDB<ReleaseData>(urls.release)
+  const { data: credits, loading: creditsLoading, error: creditsError } = useFetchTMDB<object>(urls.credits)
+  const { data: reviews, loading: reviewsLoading, error: reviewsError } = useFetchTMDB<object>(urls.reviews)
+  const { data: similar, loading: similarLoading, error: similarError } = useFetchTMDB<object>(urls.similar)
+  const { data: videos, loading: videosLoading, error: videosError } = useFetchTMDB<object>(urls.videos)
+
+  if (titleLoading || releaseLoading || creditsLoading || reviewsLoading || similarLoading || videosLoading) {
+    return <>Loading</>
+  }
+
+  if (titleError || releaseError || creditsError || reviewsError || similarError || videosError) {
+    return <>Error</>
+  }
+
+  if (!title || !release || !credits || !reviews || !similar || !videos) return null
 
   return (
     <>
       <Navigation />
-      <section className='mt-28'>
-        <Movie id={params.id} />
+      <section className='mt-16'>
+        <Title title={title} release={release} credits={credits} reviews={reviews} similar={similar} videos={videos} />
       </section>
+      <Footer />
     </>
   )
 }
--- src/pages/Main/Series.tsx
+++ src/pages/Main/Series.tsx
@@ -1,11 +1,52 @@
 import Navigation from '../../components/Header/Navigation'
+import Title from '../../components/Title/Title'
 import Footer from '../../components/Footer/Footer'
+import { useParams } from 'react-router-dom'
+import { useFetchTMDB } from '../../hooks/useFetchTMDB'
+import { ReleaseData } from '../../components/Title/types/ReleaseData'
+import { TitleData } from '../../components/Title/types/TitleData'
+
+type IParams = {
+  id?: string
+}
 
 const Series: React.FC = () => {
+  const params = useParams<IParams>()
+  const locale: string = navigator.language
+  const id = params.id
+
+  const urls = {
+    data: `https://api.themoviedb.org/3/tv/${id}?language=${locale}`,
+    release: `https://api.themoviedb.org/3/tv/${id}/release_dates`,
+    credits: `https://api.themoviedb.org/3/tv/${id}/credits?language=${locale}`,
+    reviews: `https://api.themoviedb.org/3/tv/${id}/reviews?language=${locale}&page=1`,
+    similar: `https://api.themoviedb.org/3/tv/${id}/similar?language=${locale}&page=1`,
+    videos: `https://api.themoviedb.org/3/tv/${id}/videos?language=${locale}`,
+  }
+
+  const { data: title, loading: titleLoading, error: titleError } = useFetchTMDB<TitleData>(urls.data)
+  const { data: release, loading: releaseLoading, error: releaseError } = useFetchTMDB<ReleaseData>(urls.release)
+  const { data: credits, loading: creditsLoading, error: creditsError } = useFetchTMDB<object>(urls.credits)
+  const { data: reviews, loading: reviewsLoading, error: reviewsError } = useFetchTMDB<object>(urls.reviews)
+  const { data: similar, loading: similarLoading, error: similarError } = useFetchTMDB<object>(urls.similar)
+  const { data: videos, loading: videosLoading, error: videosError } = useFetchTMDB<object>(urls.videos)
+
+  if (titleLoading || releaseLoading || creditsLoading || reviewsLoading || similarLoading || videosLoading) {
+    return <>Loading</>
+  }
+
+  if (titleError || releaseError || creditsError || reviewsError || similarError || videosError) {
+    return <>Error</>
+  }
+
+  if (!title || !release || !credits || !reviews || !similar || !videos) return null
+
   return (
     <>
       <Navigation />
-      <section className='mt-16'>components about move</section>
+      <section className='mt-16'>
+        <Title title={title} release={release} credits={credits} reviews={reviews} similar={similar} videos={videos} />
+      </section>
       <Footer />
     </>
   )
