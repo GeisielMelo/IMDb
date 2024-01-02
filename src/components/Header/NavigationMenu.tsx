d4a07520bff6acce2fa4fb3628ed8e5c20e17e08
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-02 17:51:32 -0300

    feat: Added "My List" functionality to the navbar.

--- src/components/Header/NavigationMenu.tsx
+++ src/components/Header/NavigationMenu.tsx
@@ -1,34 +1,54 @@
 import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
 
 const menu = [
   { option: 'Movies', url: '' },
   { option: 'TV Show', url: '' },
   { option: 'People', url: '' },
-  { option: 'My List', url: '/my-list' },
 ]
 
 export const NavigationFlex: React.FC = () => {
+  const { user } = useAuth()
   const navigate = useNavigate()
   return (
     <ul className='hidden gap-6 md:flex'>
       {menu.map((_, key) => (
-        <li className='cursor-pointer' key={key} onClick={() => navigate(_.url)}>
+        <li
+          className='cursor-pointer'
+          key={key}
+          onClick={() => navigate(_.url)}
+        >
           {_.option}
         </li>
       ))}
+      {user && (
+        <li className='cursor-pointer' onClick={() => navigate('/my-list')}>
+          My List
+        </li>
+      )}
     </ul>
   )
 }
 
 export const NavigationBox: React.FC = () => {
+  const { user } = useAuth()
   const navigate = useNavigate()
   return (
     <ul className='absolute flex flex-col gap-2 md:hidden px-8 py-2 w-full bg-white z-10'>
       {menu.map((_, key) => (
-        <li className='cursor-pointer' key={key} onClick={() => navigate(_.url)}>
+        <li
+          className='cursor-pointer'
+          key={key}
+          onClick={() => navigate(_.url)}
+        >
           {_.option}
         </li>
       ))}
+      {user && (
+        <li className='cursor-pointer' onClick={() => navigate('/my-list')}>
+          My List
+        </li>
+      )}
     </ul>
   )
 }
