1f4a09695c0444190d333cb8f875ec50a230b505
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-03 14:46:25 -0300

    raw: added some screens and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss'
+
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        'transparent-black': 'rgba(15, 15, 15, 0.9)',
+      },
+    },
   },
   plugins: [],
 }
