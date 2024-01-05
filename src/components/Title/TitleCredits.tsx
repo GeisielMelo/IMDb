e2eeeb83448cbddd637d23757ce49fc279b7e039
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-05 00:04:15 -0300

    feat: Updated credits to show only data that has a background image.

--- src/components/Title/TitleCredits.tsx
+++ src/components/Title/TitleCredits.tsx
@@ -14,17 +14,20 @@ const TitleCredits: React.FC<CreditsProps> = ({ credits }) => {
     <div className='max-w-5xl w-full'>
       <h1 className='mb-6 text-xl font-semibold'>Top Cast</h1>
       <div className=' grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-2'>
-        {credits.cast.slice(0, maxCards).map((element, index) => (
-          <div className='max-w-[150px] shadow rounded-lg' key={index}>
-            <img
-              className='object-contain w-full rounded-t-lg'
-              src={url + element.profile_path}
-              alt={`${element.name} as ${element.character}.`}
-            />
-            <h1>{element.name}</h1>
-            <p>{element.character}</p>
-          </div>
-        ))}
+        {credits.cast.slice(0, maxCards).map(
+          (element, key) =>
+            element.profile_path && (
+              <div className='max-w-[150px] shadow rounded-lg' key={key}>
+                <img
+                  className='object-contain w-full rounded-t-lg'
+                  src={url + element.profile_path}
+                  alt={`${element.name} as ${element.character}.`}
+                />
+                <h1>{element.name}</h1>
+                <p>{element.character}</p>
+              </div>
+            ),
+        )}
       </div>
       <div className='text-center mt-6'>
         {!isMaxCardReached && (
