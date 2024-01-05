05eb651041a8fe024b370d85dcb77ae8bbd75b83
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-05 00:07:59 -0300

    feat: Removed the Movies and TV Show options from the navigation menu.

--- src/components/Header/NavigationMenu.tsx
+++ src/components/Header/NavigationMenu.tsx
@@ -1,10 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
 
-const menu = [
-  { option: 'Movies', url: '' },
-  { option: 'TV Show', url: '' },
-]
+const menu = [{ option: 'Home', url: '/' }]
 
 export const NavigationFlex: React.FC = () => {
   const { user } = useAuth()
@@ -12,11 +9,7 @@ export const NavigationFlex: React.FC = () => {
   return (
     <ul className='hidden gap-6 md:flex'>
       {menu.map((_, key) => (
-        <li
-          className='cursor-pointer'
-          key={key}
-          onClick={() => navigate(_.url)}
-        >
+        <li className='cursor-pointer' key={key} onClick={() => navigate(_.url)}>
           {_.option}
         </li>
       ))}
@@ -35,11 +28,7 @@ export const NavigationBox: React.FC = () => {
   return (
     <ul className='absolute flex flex-col gap-2 md:hidden px-8 py-2 w-full bg-white z-10'>
       {menu.map((_, key) => (
-        <li
-          className='cursor-pointer'
-          key={key}
-          onClick={() => navigate(_.url)}
-        >
+        <li className='cursor-pointer' key={key} onClick={() => navigate(_.url)}>
           {_.option}
         </li>
       ))}
