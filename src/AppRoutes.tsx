88eb6caee7aaa0aea31760c2d026c4f3ce3eb1c9
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-05 00:12:27 -0300

    feat: Deleted the 'Movies' and 'Series' components and created a main one called Title to reduce code complexity.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,10 +6,9 @@ import Home from './pages/Main/Home'
 import Login from './pages/Sign/Login'
 import Register from './pages/Sign/Register'
 import List from './pages/Main/List'
-import NotFound from './pages/Error/NotFound'
+import NotFound from './components/Error/NotFound'
 import Search from './pages/Main/Search'
-import Series from './pages/Main/Series'
-import Movies from './pages/Main/Movies'
+import Titles from './pages/Main/Titles'
 
 type RoutesProps = {
   children: ReactNode
@@ -19,23 +18,11 @@ const AppRoutes: React.FC = () => {
   const { authenticated, loading } = useAuth()
 
   const Private: React.FC<RoutesProps> = ({ children }) => {
-    return loading ? (
-      <span>Loading...</span>
-    ) : authenticated ? (
-      children
-    ) : (
-      <Navigate to='/sign-in' />
-    )
+    return loading ? <span>Loading...</span> : authenticated ? children : <Navigate to='/sign-in' />
   }
 
   const Public: React.FC<RoutesProps> = ({ children }) => {
-    return loading ? (
-      <span>Loading...</span>
-    ) : authenticated ? (
-      <Navigate to='/' />
-    ) : (
-      children
-    )
+    return loading ? <span>Loading...</span> : authenticated ? <Navigate to='/' /> : children
   }
 
   return (
@@ -44,8 +31,7 @@ const AppRoutes: React.FC = () => {
         <Route path={'/'} element={<Home />} />
         <Route path={'*'} element={<NotFound />} />
         <Route path={'/search'} element={<Search />} />
-        <Route path={'/tv/:id'} element={<Series />} />
-        <Route path={'/movie/:id'} element={<Movies />} />
+        <Route path={'/:type/:id'} element={<Titles />} />
         <Route path={'sign-in'} element={<Public><Login /></Public>}/>
         <Route path={'sign-up'} element={<Public><Register /></Public>}/>
         <Route path={'/my-list'} element={<Private><List /></Private>}/>
---
+++ src/pages/Main/Titles.tsx
@@ -0,0 +1,85 @@
+import Navigation from '../../components/Header/Navigation'
+import Title from '../../components/Title/Title'
+import Footer from '../../components/Footer/Footer'
+import { useParams } from 'react-router-dom'
+import { useFetchTMDB } from '../../hooks/useFetchTMDB'
+import { ReleaseData } from '../../types/ReleaseData'
+import { TitleData } from '../../types/TitleData'
+import { CreditsData } from '../../types/CreditsData'
+import { ReviewsData } from '../../types/ReviewsData'
+import { VideosData } from '../../types/VideosData'
+import { SimilarData } from '../../types/SimilarData'
+import NotFound from '../../components/Error/NotFound'
+import { LoadingSpinner } from '../../components/animated/LoadingSpinner'
+
+type IParams = {
+  id?: string
+  type?: string
+}
+
+const Titles: React.FC = () => {
+  const params = useParams<IParams>()
+  const locale: string = navigator.language
+  const { type, id } = params
+
+  const urls = {
+    data: `https://api.themoviedb.org/3/${type}/${id}?language=${locale}`,
+    release: `https://api.themoviedb.org/3/${type}/${id}/release_dates`,
+    credits: `https://api.themoviedb.org/3/${type}/${id}/credits?language=${locale}`,
+    reviews: `https://api.themoviedb.org/3/${type}/${id}/reviews?language=${locale}&page=1`,
+    similar: `https://api.themoviedb.org/3/${type}/${id}/similar?language=${locale}&page=1`,
+    videos: `https://api.themoviedb.org/3/${type}/${id}/videos?language=${locale}`,
+  }
+
+  const { data: title, loading: titleLoading, error: titleError } = useFetchTMDB<TitleData>(urls.data)
+  const { data: release, loading: releaseLoading, error: releaseError } = useFetchTMDB<ReleaseData>(urls.release)
+  const { data: credits, loading: creditsLoading, error: creditsError } = useFetchTMDB<CreditsData>(urls.credits)
+  const { data: reviews, loading: reviewsLoading, error: reviewsError } = useFetchTMDB<ReviewsData>(urls.reviews)
+  const { data: similar, loading: similarLoading, error: similarError } = useFetchTMDB<SimilarData>(urls.similar)
+  const { data: videos, loading: videosLoading, error: videosError } = useFetchTMDB<VideosData>(urls.videos)
+
+  const loading = titleLoading || releaseLoading || creditsLoading || reviewsLoading || similarLoading || videosLoading
+  const error = titleError || releaseError || creditsError || reviewsError || similarError || videosError
+
+  if (loading) {
+    return (
+      <>
+        <Navigation />
+        <div className='flex items-center justify-center h-[100dvh]'>
+          <LoadingSpinner />
+        </div>
+        <Footer />
+      </>
+    )
+  }
+
+  if (error) {
+    return (
+      <>
+        <Navigation />
+        <NotFound />
+        <Footer />
+      </>
+    )
+  }
+
+  if (!title || !release || !credits || !reviews || !similar || !videos) return null
+
+  return (
+    <>
+      <Navigation />
+      <Title 
+        title={title}
+        release={release}
+        credits={credits}
+        reviews={reviews}
+        similar={similar}
+        videos={videos}
+        loading={loading}
+      />
+      <Footer />
+    </>
+  )
+}
+
+export default Titles
