2c38e672a16df4332f807719422570f16c556c6b
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-05 00:02:05 -0300

    feat: Updated types on TitleData.

--- src/components/Carousel/Card.tsx
+++ src/components/Carousel/Card.tsx
@@ -9,9 +9,19 @@ type Card = {
   title: string | undefined
   note: string | undefined
   added: boolean | undefined
+  watchList: boolean | undefined
 }
 
-export const Card: React.FC<Card> = ({ handleRedirect, handleWatch, src, alt, title, added, note }) => {
+export const Card: React.FC<Card> = ({
+  handleRedirect,
+  handleWatch,
+  src,
+  alt,
+  title,
+  added,
+  note,
+  watchList = true,
+}) => {
   const [isImageLoaded, setIsImageLoaded] = useState(false)
 
   const handleImageLoad = () => {
@@ -21,8 +31,18 @@ export const Card: React.FC<Card> = ({ handleRedirect, handleWatch, src, alt, ti
   return (
     <>
       <div className='relative mx-[0.25rem] aspect-[9/13] hover:cursor-pointer'>
-        <img src={src} alt={alt} onLoad={handleImageLoad} className='aspect-[9/13] rounded-lg' onClick={handleRedirect} />
-        <div className={`absolute top-0 left-0 w-full h-full bg-slate-400 ${isImageLoaded && 'hidden'}`}>
+        <img
+          src={src}
+          alt={alt}
+          onLoad={handleImageLoad}
+          className='aspect-[9/13] rounded-lg'
+          onClick={handleRedirect}
+        />
+        <div
+          className={`absolute top-0 left-0 w-full h-full bg-slate-400 ${
+            isImageLoaded && 'hidden'
+          }`}
+        >
           <div className='animate-pulse w-full h-full bg-slate-500' />
         </div>
       </div>
@@ -33,12 +53,14 @@ export const Card: React.FC<Card> = ({ handleRedirect, handleWatch, src, alt, ti
           <Star className='h-4' />
         </div>
         <div className='h-20'>{title}</div>
-        <button
-          className='flex justify-center items-center rounded-full w-full py-1 text-[0.875rem] font-normal tracking-wide border border-zinc-500'
-          onClick={handleWatch}
-        >
-          {added ? <MinusCircle className='h-4' /> : <PlusCircle className='h-4' />} WATCHLIST
-        </button>
+        {watchList && (
+          <button
+            className='flex justify-center items-center rounded-full w-full py-1 text-[0.875rem] font-normal tracking-wide border border-zinc-500'
+            onClick={handleWatch}
+          >
+            {added ? <MinusCircle className='h-4' /> : <PlusCircle className='h-4' />} WATCHLIST
+          </button>
+        )}
       </div>
     </>
   )
--- src/types/TitleData.ts
+++ src/types/TitleData.ts
@@ -2,15 +2,22 @@
 export type TitleData = {
   success: boolean
   genres: { [key: string]: string }[]
-  original_title: string
+  genre_ids: number[]
+  runtime: number
+  original_title?: string
+  original_language: string
   name: string
   title: string
   first_air_date: string
   release_date: string
   backdrop_path: string
   poster_path: string
-  runtime: number
   vote_average: number
   tagline: string
   overview: string
+  id:number
+  vote_count: number
+  popularity: number
+  adult: boolean
+  media_type: string
 }
