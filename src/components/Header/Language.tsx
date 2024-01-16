d991a4478bf4ef820b097cfb179a8e0eb537dfa8
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-16 15:17:00 -0300

    feat: Created a component to handle setting the app language.

---
+++ src/components/Header/Language.tsx
@@ -0,0 +1,8 @@
+export const Language: React.FC = () => {
+  return (
+    <select className='hidden md:block border-none bg-[rgba(18,18,18,255)] text-white'>
+      <option value='en-US'>EN</option>
+      <option value='pt-BR'>PT</option>
+    </select>
+  )
+}
