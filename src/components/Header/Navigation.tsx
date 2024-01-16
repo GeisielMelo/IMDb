dc94768e9f0756f796aaed08f3a9444a6a64a5d2
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-16 13:28:11 -0300

    feat: navigation rebuild

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -2,59 +2,54 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext'
 import { SearchBar } from './SearchBar'
-import { Menu, X, LogOut, Search } from 'lucide-react'
+import { Menu, X, LogOut, Search, BookmarkPlus, Languages } from 'lucide-react'
 import logo from '../../assets/svg/logo.svg'
 import { NavigationFlex, NavigationBox } from './NavigationMenu'
 
 const Navigation: React.FC = () => {
   const navigate = useNavigate()
-  const { user, logout } = useAuth()
+  const { authenticated, logout } = useAuth()
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
   const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false)
 
-  const handleLogOut = async () => {
-    try {
-      await logout()
-    } catch (error) {
-      console.error('Error on Logout.')
-    }
-  }
-
-  const handleOpenMenu = () => {
-    if (isSearchOpen) setIsSearchOpen(false)
-    setIsMenuOpen(!isMenuOpen)
-  }
-
-  const handleOpenSearch = () => {
-    if (isMenuOpen) setIsMenuOpen(false)
-    setIsSearchOpen(!isSearchOpen)
-  }
-
   return (
-    <nav className='flex items-center justify-center w-full bg-black text-white'>
-      <div className='max-w-7xl w-full'>
-        <div className='flex justify-between items-center py-4 px-8'>
-          <div className='flex items-center gap-6'>
-            <img className='h-8 cursor-pointer' src={logo} onClick={() => navigate('/')} alt='Logo' />
-            <NavigationFlex />
-          </div>
-
-          <div className='flex gap-6'>
-            {user ? (
-              <button onClick={() => handleLogOut()}>
-                <LogOut />
-              </button>
-            ) : (
-              <button onClick={() => navigate('/sign-in')}>Login</button>
-            )}
-            <button onClick={() => handleOpenSearch()}>{isSearchOpen ? <X /> : <Search />}</button>
-            <button className='block md:hidden' onClick={() => handleOpenMenu()}>
-              <Menu />
-            </button>
-          </div>
+    <nav className='flex items-center justify-center w-full text-md px-2 bg-black text-white'>
+      
+      <div className='flex items-center max-w-7xl w-full my-3 gap-2'>
+        <div className='flex flex-row-reverse items-center md:flex-row'>
+          <h1 className='px-2 rounded'>TMDb</h1>
+          <button className='flex px-4 py-1 gap-1'>
+            <Menu />
+            <span className='hidden md:block'>Menu</span>
+          </button>
         </div>
-        {isMenuOpen && <NavigationBox />}
-        {isSearchOpen && <SearchBar />}
+
+        <div className='w-full flex rounded justify-end md:justify-normal bg-transparent md:bg-white'>
+          <input className='hidden md:block w-full rounded-l' type='text' name='search' />
+          <button className='hidden md:block mx-4 my-1 text-zinc-500'>
+            <Search />
+          </button>
+          <button className='block md:hidden'>
+            <Search />
+          </button>
+        </div>
+
+        <button className='hidden md:flex ml-2'>
+          <BookmarkPlus />
+          <span>Watchlist</span>
+        </button>
+
+        <button
+          className='px-4 text-md whitespace-nowrap'
+          onClick={() => (authenticated ? logout() : navigate('/sign-in'))}
+        >
+          {authenticated ? <span>Sign Out</span> : <span>Sign In</span>}
+        </button>
+
+        <select className='hidden md:block border-none bg-black text-white'>
+          <option value='en-US'>EN</option>
+          <option value='pt-BR'>PT</option>
+        </select>
       </div>
     </nav>
   )
