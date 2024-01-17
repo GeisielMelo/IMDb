727c177dc00f2faeba2050f61dbf95eba0e8d27b
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-17 14:46:02 -0300

    feat: Created utils functions to format the data from responses.

---
+++ src/utils/TheMovieDbUtils.ts
@@ -0,0 +1,21 @@
+import { MovieData } from "../types/MovieData"
+
+export const handleGetTitleName = (element: MovieData): string => {
+  if (element.title) return element.title
+  if (element.name) return element.name
+  if (element.original_title) return element.original_title
+  else return 'Movie'
+}
+
+export const handleGetTitlePoster = (element: MovieData): string => {
+  const path = element.poster_path
+  const url = `https://image.tmdb.org/t/p/w500${path}`
+  const notFoundUrl = '/backgroundNotFound.jpg'
+  return path ? url : notFoundUrl
+}
+
+export const handleGetTitleVote = (element: MovieData): number => {
+  const vote = element.vote_average
+  const noVote = 0.0
+  return vote ? parseInt(vote.toFixed(1)) : noVote
+}
