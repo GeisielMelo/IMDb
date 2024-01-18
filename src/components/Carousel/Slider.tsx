04c89e51cbc000e1d9f1fcfbdbe56b43addb0271
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-18 15:22:19 -0300

    style: complete rebuild card component to make it rausable.

--- src/components/Carousel/Slider.tsx
+++ src/components/Carousel/Slider.tsx
@@ -1,89 +1,41 @@
-import { useMovies } from '../../context/MovieContext'
-import { useNavigate } from 'react-router-dom'
+import 'swiper/css'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { params } from '../../config/swiper'
 import { register } from 'swiper/element/bundle'
 import { MovieData } from '../../types/MovieData'
 import { Prev, Next } from './Navigation'
-import { ImageSkeleton } from './Skeletons'
-import { Card } from './Card'
-import 'swiper/css'
+import { Card } from '../Card/Card'
 import { useFetchTMDB } from '../../hooks/useFetchTMDB'
-import { useDisplayAlert } from '../../hooks/useDisplayAlert'
-import { useAuth } from '../../context/AuthContext'
+import { handleGetTitleName, handleGetTitlePoster, handleGetTitleVote } from '../../utils/handleFunctionsUtils'
 register()
 
 type SliderTypes = {
   category: string
   url: string
-  id: string
+  sectionName: string
 }
 
 type ResultTypes = {
   results: MovieData[]
 }
 
-const Slider: React.FC<SliderTypes> = ({ id, category, url }) => {
-  const navigate = useNavigate()
-  const { Alert, displayAlert } = useDisplayAlert()
+const Slider: React.FC<SliderTypes> = ({ sectionName, category, url }) => {
   const { data, loading, error } = useFetchTMDB<ResultTypes>(url)
-  const { movies, addMovie, removeMovie } = useMovies()
-  const { authenticated } = useAuth()
 
-  if (error) navigate('/404')
-  if (!data || !Array.isArray(data.results)) return
-  const results: MovieData[] = data.results
+  if (error || !data) return null
 
-  const handlePosterPath = (element: MovieData): string => {
-    const path = element.poster_path
-    const url = `https://image.tmdb.org/t/p/w500${path}`
-    const notFoundUrl = '/backgroundNotFound.jpg'
-    return path ? url : notFoundUrl
-  }
-
-  const handleFormatVoteAverage = (element: MovieData): string => {
-    if (element.vote_average === 0) return 'Upcoming'
-    else return Number(element.vote_average).toPrecision(2)
-  }
-
-  const handleSetName = (element: MovieData) => {
-    if (element.name) return element.name
-    if (element.title) return element.title
-    if (element.original_title) return element.original_title
-    if (element.original_name) return element.original_name
-  }
-
-  const handleAlreadyAdded = (element: MovieData): boolean => {
-    return movies.some((item: MovieData) => item.id === element.id)
-  }
-
-  const handleLikeMovie = (element: MovieData) => {
-    if (!authenticated) {
-      displayAlert('You must be connected to perform this action.', 'info')
-    }
-
-    const alreadyExists = handleAlreadyAdded(element)
-    alreadyExists ? removeMovie(element) : addMovie(element)
-  }
-
-  const handleRedirect = (element: MovieData) => {
-    if (element.media_type === 'tv') {
-      return navigate(`/tv/${element.id}`)
-    } else {
-      return navigate(`/movie/${element.id}`)
-    }
-  }
+  const results = data.results
 
   return (
-    <section id={id} className='my-8'>
-      <h1 className='pl-8 font-sans font-medium'>{category}</h1>
-      <Swiper {...params} className='px-8 relative flex'>
+    <section id={sectionName} className='my-8'>
+      <h1 className='pl-8 mb-2 font-sans font-medium'>{category}</h1>
+      <Swiper {...params} className='px-8 grid grid-cols-1'>
         <Prev />
         {loading ? (
           <>
             {[...Array(8)].map((_, index) => (
               <SwiperSlide key={index}>
-                <ImageSkeleton />
+                <>Loading...</>
               </SwiperSlide>
             ))}
           </>
@@ -91,23 +43,23 @@ const Slider: React.FC<SliderTypes> = ({ id, category, url }) => {
           <>
             {results.map((element, key) => (
               <SwiperSlide key={key}>
-                <Card
-                  handleRedirect={() => handleRedirect(element)}
-                  handleWatch={() => handleLikeMovie(element)}
-                  src={handlePosterPath(element)}
-                  alt={handleSetName(element)}
-                  title={handleSetName(element)}
-                  note={handleFormatVoteAverage(element)}
-                  added={handleAlreadyAdded(element)}
-                  watchList={true}
-                />
+                <div className='mx-[0.25rem]'>
+                  <Card
+                    key={key}
+                    id={element.id}
+                    src={handleGetTitlePoster(element)}
+                    title={handleGetTitleName(element)}
+                    vote={handleGetTitleVote(element)}
+                    media={element.media_type}
+                    element={element}
+                  />
+                </div>
               </SwiperSlide>
             ))}
           </>
         )}
         <Next />
       </Swiper>
-      <Alert />
     </section>
   )
 }
