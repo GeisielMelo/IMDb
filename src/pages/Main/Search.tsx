491d2ca4d473b82344198c2122509adb96d6c4c7
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-17 23:49:02 -0300

    feat: Fully rebuilt the search section.

--- src/pages/Main/Search.tsx
+++ src/pages/Main/Search.tsx
@@ -1,76 +1,27 @@
 import { useLocation } from 'react-router-dom'
 import { useLanguages } from '../../hooks/useLanguages'
 import { useInfiniteScroll as InfiniteScroll } from '../../hooks/useInfiniteScroll'
+import { useSpreadableSearch } from '../../hooks/useSpreadableSearch'
 import { handleGetTitlePoster, handleGetTitleName, handleGetTitleVote } from '../../utils/TheMovieDbUtils'
 import { Card } from '../../components/Card/Card'
 import Navigation from '../../components/Header/Navigation'
 import Footer from '../../components/Footer/Footer'
-import { MovieData } from '../../types/MovieData'
-import { useEffect, useState } from 'react'
 
-type Headers = {
-  accept: string
-  Authorization: string
-}
-
-type Options = {
-  method: 'GET'
-  headers: Headers
-}
-
-const options: Options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_THEMOVIEDB_AUTHORIZATION}`,
-  },
-}
-
-const Search = () => {
+const Search:React.FC = () => {
   const { locale } = useLanguages()
   const { search } = useLocation()
 
-  const [page, setPage] = useState(1)
-  const [maxPage, setMaxPage] = useState(0)
-  const [data, setData] = useState<MovieData[] | null>(null)
-  const [loading, setLoading] = useState(true)
-
   const queryParams = new URLSearchParams(search)
-  const title: string | null = queryParams.get('title')
+  const query: string | null = queryParams.get('q')
   const type: string | null = queryParams.get('type')
 
-  useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      if (!title || !type) throw new Error('Missing Params.')
-
-      const formattedQuery = title.replace(' ', '%20')
-      const url = `https://api.themoviedb.org/3/search/${type}?query=${formattedQuery}&include_adult=false&language=${locale}&page=${page}`
-
-      try {
-        const response = await fetch(url, options)
-        const result = await response.json()
-
-        if (data) {
-          setData([...data, ...result.results])
-        } else {
-          setData(result.results)
-        }
-
-        if (maxPage == 0) setMaxPage(result.total_pages)
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchData()
-  }, [page])
+  const { page, maxPage, data, loading, loadingMore, setPage } = useSpreadableSearch(query, type, locale)
 
   return (
     <>
       <Navigation />
       <section className='flex flex-col min-h-screen items-center justify-center px-2 md:px-8 pt-4 pb-8'>
-        <h1 className='max-w-7xl w-full mb-4'>Watchlist</h1>
+        <h1 className='max-w-7xl w-full mb-4'>Search: {query}.</h1>
         <div className='grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4 max-w-7xl w-full'>
           {loading ? (
             <>
@@ -89,17 +40,17 @@ const Search = () => {
                       src={handleGetTitlePoster(element)}
                       title={handleGetTitleName(element)}
                       vote={handleGetTitleVote(element)}
-                      media={element.media_type}
+                      media={type}
                     />
                   ))}
                 </>
               ) : (
-                <div>No results for this search.</div>
+                <div className='h-screen'>No results.</div>
               )}
             </>
           )}
         </div>
-        {page < maxPage && data && !loading && <InfiniteScroll fetchMore={() => setPage(page + 1)} />}
+        {page < maxPage && data && !loading && !loadingMore && <InfiniteScroll fetchMore={() => setPage(page + 1)} />}
       </section>
       <Footer />
     </>
