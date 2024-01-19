d53dfad63e183afd5f9edee424332cd40d5393fe
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-19 00:06:26 -0300

    feat: Moved handle functions to the utils module to be used in other components.

--- src/utils/handleFunctionsUtils.ts
+++ src/utils/handleFunctionsUtils.ts
@@ -1,4 +1,6 @@
 import { MovieData } from '../types/MovieData'
+import { TitleData } from '../types/TitleData'
+import { VideosData } from '../types/VideosData'
 
 export const handleGetTitleName = (element: MovieData): string => {
   if (element.title) return element.title
@@ -27,4 +29,50 @@ export const handleAddMediaType = (data: MovieData[], type: string): MovieData[]
     }
     return element
   })
+}
+
+export const handleGetTitleReleaseYear = (element: TitleData) => {
+  const date = element.first_air_date || element.release_date
+  if (!date) return ''
+  const year = date.split('-')[0]
+  return `(${year})`
+}
+
+export const handleTitleDuration = (minutes: number) => {
+  const hh = Math.floor(minutes / 60)
+  const mm = minutes % 60
+  return `${hh}h ${mm}m`
+}
+
+export const handleGetTitleKey = (element: VideosData) => {
+  const trailers = element.results.filter((_) => _.type === 'Trailer' && _.official)
+  const teasers = element.results.filter((_) => _.type === 'Teaser' && _.official)
+
+  if (trailers.length) {
+    const randomIndex = Math.floor(Math.random() * trailers.length)
+    const randomTrailer = trailers[randomIndex]
+    return randomTrailer.key
+  } else {
+    const randomIndex = Math.floor(Math.random() * teasers.length)
+    const randomTeaser = teasers[randomIndex]
+    return randomTeaser.key
+  }
+}
+
+export const handleTitleHasVideo = (element: VideosData) => {
+  const trailers = element.results.filter((_) => _.type === 'Trailer' && _.official)
+  const teasers = element.results.filter((_) => _.type === 'Teaser' && _.official)
+  const hasTrailers = !!trailers.length
+  const hasTeasers = !!teasers.length
+
+  if (hasTrailers || hasTeasers) {
+    return true
+  }
+  return false
+}
+
+export const handleVoteAverageToPercent = (vote: number) => {
+  if (!vote) return 0
+  const fixedVote = parseFloat(vote.toFixed(2))
+  return Math.round(fixedVote * 10)
 }
