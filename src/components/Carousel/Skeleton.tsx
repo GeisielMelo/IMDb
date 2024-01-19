a6ec510925901a83245fc5da9a147ae75bc4c649
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-19 14:27:17 -0300

    feat: new card skeleton

---
+++ src/components/Carousel/Skeleton.tsx
@@ -0,0 +1,8 @@
+export const CardSkeleton: React.FC = () => {
+  return (
+    <div className='mx-[0.25rem] rounded animate-pulse bg-zinc-400'>
+      <div className="aspect-[9/13]"/>
+      <div className='h-36'/>
+    </div>
+  )
+}
