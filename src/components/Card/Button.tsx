cf9be88ca06a802953d97f35da1106dbeeee42f9
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-20 15:50:28 -0300

    fix: Fixed undefined parameter when the title is added through the similar component.

--- src/components/Card/Button.tsx
+++ src/components/Card/Button.tsx
@@ -3,6 +3,7 @@ import { useDisplayAlert } from '../../hooks/useDisplayAlert'
 import { useMovies } from '../../context/MovieContext'
 import { useAuth } from '../../context/AuthContext'
 import { MovieData } from '../../types/MovieData'
+import { useParams } from 'react-router-dom'
 
 type Button = {
   element: MovieData
@@ -12,11 +13,18 @@ export const Button: React.FC<Button> = ({ element }) => {
   const { movies, addMovie, removeMovie } = useMovies()
   const { Alert, displayAlert } = useDisplayAlert()
   const { authenticated } = useAuth()
+  const params = useParams()
   const isAdded = movies.some((_) => _.id === element.id)
 
   const handleLikeMovie = () => {
     if (!authenticated) return displayAlert('You must be connected to perform this action.', 'info')
-    return isAdded ? removeMovie(element) : addMovie(element)
+
+    if (params) {
+      const newData = { ...element, media_type: params.type as string }
+      return isAdded ? removeMovie(newData) : addMovie(newData)
+    } else {
+      return isAdded ? removeMovie(element) : addMovie(element)
+    }
   }
 
   return (
