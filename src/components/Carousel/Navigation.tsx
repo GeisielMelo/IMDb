b2eb6e60807c29d09c0e68dd1d2df63e3b391cf7
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-02 20:17:16 -0300

    style: updated navigation arrows.

--- src/components/Carousel/Navigation.tsx
+++ src/components/Carousel/Navigation.tsx
@@ -5,7 +5,7 @@ export const Prev: React.FC = () => {
   const swiper = useSwiper()
   return (
     <button
-      className='absolute left-0 w-[1.8rem] h-full z-10 bg-transparent-silver bg-white'
+      className='absolute left-0 w-[1.8rem] h-full z-10 bg-transparent-silver rounded-br rounded-tr bg-black/30 text-white'
       onClick={() => swiper.slidePrev()}
     >
       <ChevronLeft />
@@ -17,7 +17,7 @@ export const Next: React.FC = () => {
   const swiper = useSwiper()
   return (
     <button
-      className='absolute right-0 w-[1.8rem] h-full z-10 bg-transparent-silver bg-white'
+      className='absolute right-0 w-[1.8rem] h-full z-10 bg-transparent-silver rounded-bl rounded-tl bg-black/30 text-white'
       onClick={() => swiper.slideNext()}
     >
       <ChevronRight />
