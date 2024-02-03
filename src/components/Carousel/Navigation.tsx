c9ac07c30f33fa9f58d9f0b49ac5812476758def
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-03 12:32:29 -0300

    style: Navigation arrow buttons now have an animated transition and color.

--- src/components/Carousel/Navigation.tsx
+++ src/components/Carousel/Navigation.tsx
@@ -5,7 +5,7 @@ export const Prev: React.FC = () => {
   const swiper = useSwiper()
   return (
     <button
-      className='absolute left-0 w-[1.8rem] h-full z-10 bg-transparent-silver rounded-br rounded-tr bg-black/30 text-white'
+      className='absolute left-0 w-[1.8rem] h-full z-10 rounded-br rounded-tr bg-black/30 text-transparent hover:text-zinc-300 hover:bg-black/50 transition-all'
       onClick={() => swiper.slidePrev()}
     >
       <ChevronLeft />
@@ -17,7 +17,7 @@ export const Next: React.FC = () => {
   const swiper = useSwiper()
   return (
     <button
-      className='absolute right-0 w-[1.8rem] h-full z-10 bg-transparent-silver rounded-bl rounded-tl bg-black/30 text-white'
+      className='absolute right-0 w-[1.8rem] h-full z-10 rounded-bl rounded-tl bg-black/30 text-transparent hover:text-zinc-300 hover:bg-black/50 transition-all'
       onClick={() => swiper.slideNext()}
     >
       <ChevronRight />
