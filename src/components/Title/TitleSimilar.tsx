da61d7ba1d87470186d410637ef70eb31ca6dc18
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-02-03 12:30:45 -0300

    style: Updated elements positions and CSS.

--- src/components/Title/TitleSimilar.tsx
+++ src/components/Title/TitleSimilar.tsx
@@ -11,7 +11,7 @@ export const TitleSimilar: React.FC<Similar> = ({ locale, type, id }) => {
 
   return (
     <section className='flex item-center justify-center'>
-      <div className='max-w-7xl w-full my-4'>
+      <div className='my-4'>
         <Slider sectionName='similar' category='Similar' url={url} />
       </div>
     </section>
