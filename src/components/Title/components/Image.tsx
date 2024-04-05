8e0aa974f234a7610590e6a25a94240a74296218
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-04-05 16:06:47 -0300

    feat: added lazy loading on the image

--- src/components/Title/components/Image.tsx
+++ src/components/Title/components/Image.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import imgNotFound from '../../../assets/img/imgNotFound.jpg'
 
 type Image = {
@@ -6,13 +7,22 @@ type Image = {
 }
 
 export const Image: React.FC<Image> = ({ title, src }) => {
+  const [imageLoaded, setImageLoaded] = useState(false)
   const url = 'https://image.tmdb.org/t/p/w500'
   const setAlt = title ? `A poster about: ${title}` : 'The title poster.'
 
   return (
-    <div className='max-w-[200px] md:max-w-[300px] w-full'>
-      <img className='rounded-lg shadow-lg aspect-[9/13]' src={src ? url + src : imgNotFound} alt={setAlt} />
-      <img src='' alt='' />
-    </div>
+    <>
+      <img
+        className='rounded-lg shadow-lg aspect-[9/13] max-w-[200px] md:max-w-[300px] w-full'
+        src={src ? url + src : imgNotFound}
+        alt={setAlt}
+        onLoad={() => setImageLoaded(true)}
+        style={{ display: imageLoaded ? 'block' : 'none' }}
+      />
+      {!imageLoaded && (
+        <div className='rounded-lg max-w-[200px] md:max-w-[300px] w-full animate-pulse bg-slate-500/60 aspect-[9/13]' />
+      )}
+    </>
   )
 }
