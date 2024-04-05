30fe62670225e31b0372f75c6107161a34dc967e
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-04-05 16:06:28 -0300

    style: removed </div>

--- src/components/animated/Spinner.tsx
+++ src/components/animated/Spinner.tsx
@@ -1,3 +1,3 @@
 export const Spinner: React.FC = () => {
-  return <div className='w-8 h-8 border-dashed border-2 border-zinc-700 rounded-full animate-spin'></div>
+  return <div className='w-8 h-8 border-dashed border-2 border-zinc-700 rounded-full animate-spin'/>
 }
